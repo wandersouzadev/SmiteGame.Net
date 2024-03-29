67b80ce68de3c4333c163c9d6d70ab32210a2afd
Wander Souza <wandersouza@live.com>
2024-03-29 01:15:39 -0300

    docs: add team api docs

--- src/SmiteGame.Net/Apis/Team.cs
+++ src/SmiteGame.Net/Apis/Team.cs
@@ -12,6 +12,10 @@ namespace SmiteGame.Net.Apis
         public Team(RestClient client, HirezCredentials credentials, Options options)
             : base(client, credentials, options) { }
 
+        /// <summary>
+        /// Lists the number of players and other high level details for a particular clan.
+        /// <paramref name="clanId"/>: The ID of the clan.
+        /// </summary>
         public async Task<GetTeamDetails> GetTeamDetails(string clanId)
         {
             RestRequest request = CreateRequest("getteamdetails", clanId);
@@ -19,12 +23,20 @@ namespace SmiteGame.Net.Apis
             return response.First();
         }
 
+        /// <summary>
+        /// Lists the players for a particular clan.
+        /// <paramref name="clanId"/>: The ID of the clan.
+        /// </summary>
         public async Task<IEnumerable<GetTeamPlayers>> GetTeamPlayers(string clanId)
         {
             RestRequest request = CreateRequest("getteamplayers", clanId);
             return await HandleResponse<IEnumerable<GetTeamPlayers>>(request);
         }
 
+        /// <summary>
+        /// Returns high level information for Clan names containing the “searchTeam” string.
+        /// <paramref name="searchTeam"/>: The name of the clan to search.
+        /// </summary>
         public async Task<IEnumerable<SearchTeams>> SearchTeams(string searchTeam)
         {
             RestRequest request = CreateRequest("searchteams", searchTeam);
